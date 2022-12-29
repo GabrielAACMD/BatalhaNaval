56ff4bcbb011241e99f568789d320d04d4f69258
Gabriel <97137600+GabrielAACMD@users.noreply.github.com>
2022-12-28 22:20:59 -0300

    Update Tabuleiro.java

---
+++ Tabuleiro.java
@@ -0,0 +1,24 @@
+public class Tabuleiro{
+    static int Ordenadas = 10;
+    static int Abcissas = 10;
+    static int tabuleiro1[];
+    static int tabuleiro2[];
+
+    public static void iniciarTabuleiro(){
+        tabuleiro1 = new int [Ordenadas] [Abcissas];
+        tabuleiro2 = new int [Ordenadas] [Abcissas];
+
+        for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				tabuleiro[i][j] = '.';
+			}
+
+    }
+
+    public static void navios(){
+        
+       
+    }
+
+
+}
